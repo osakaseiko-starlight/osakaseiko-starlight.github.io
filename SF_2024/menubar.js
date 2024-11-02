d90a64186a07f32cb1daaec0ddf614de7212770b
Tsubasa <114337707+te-wing@users.noreply.github.com>
2024-11-03 00:55:05 +0900

    menubar

--- SF_2024/menubar.js
+++ SF_2024/menubar.js
@@ -1,4 +1,8 @@
 function toggleMenu() {
     const dropdown = document.querySelector('.dropdown-content');
     dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+
+    // ハンバーガーメニューの状態をトグルする
+    const hamburger = document.querySelector('.hamburger');
+    hamburger.classList.toggle('open');
 }
